4ae0f49fa8b50af8e6010927673140ab76ff68a6
Marwan-Refaat <marwan.ahmed3614@gmail.com>
2021-10-08 19:48:13 +0200

    Added database connection to app.js

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
-require('./db/mongoose')
-const port = process.env.PORT || 8000;
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
 app.get('/', (req, res) => {
     res.status(200).send("Hello, world!")
 })
 
 app.listen(port, () => {
-    console.log('Server is up on port ', port)
-})
+    console.log(`Server is running on port: ${port}`);
+});
